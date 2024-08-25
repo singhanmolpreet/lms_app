2fce5e6681a2509abb7cd45d050528ec5804129a
singhanmolpreet <singhanmolp18@gmail.com>
2024-08-25 14:36:33 +0530

    Cmain

---
+++ lib_man_sys/doctype/articles/test_articles.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Anmolpreet and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestArticles(FrappeTestCase):
+	pass
---
+++ lib_man_sys/doctype/library_member/library_member.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Anmolpreet and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+from frappe.model.document import Document 
+
+class LibraryMember(Document):
+	pass
